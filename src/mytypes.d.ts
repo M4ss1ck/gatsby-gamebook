a3ce161b971de8d676681da65ab7d421181fe40f
M4ss1ck <4ndyraul@gmail.com>
2022-06-18 02:01:11 -0400

    add: types

---
+++ src/mytypes.d.ts
@@ -0,0 +1,27 @@
+import React from 'react';
+
+interface Action {
+  id: string;
+  text: string;
+}
+
+interface IPage {
+  id: string;
+  html: string;
+  random?: boolean;
+  actions: Action[];
+}
+
+type PageDict = Record<string, IPage>;
+
+interface IContent {
+  html: string;
+}
+
+interface Links {
+  actions: Action[];
+  setPage: React.Dispatch<React.SetStateAction<IPage>>;
+  random?: boolean;
+}
+
+export { IPage, PageDict, IContent, Links };
