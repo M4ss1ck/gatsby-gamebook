480ae755e5b95639bf50181a3bc17584dcafcb86
M4ss1ck <4ndyraul@gmail.com>
2022-06-18 03:00:38 -0400

    add: adjust main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,43 @@
-import Title from '@/components/Title';
+import { useEffect, useState } from 'react';
+import DarkMode from '@/components/DarkMode';
+import Content from '@/components/Content';
+import Actions from '@/components/Actions';
+import { pages } from '@/data/pages';
 
 export default function Home() {
+  const initialIndex = window.localStorage.getItem(`page`) ?? `intro`;
+
+  const [page, setPage] = useState(pages[initialIndex]);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    if (page && page.id) window.localStorage.setItem(`page`, page.id);
+  }, [page, page.id]);
   return (
-    <main>
-      <Title>Hello TypeScript!</Title>
-      <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
-      <p>
-        Follow me on Twitter (
-        <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
-      </p>
+    <main className="font-sans px-4 py-10 text-center text-xl text-black dark:text-gray-200">
+      <DarkMode />
+      {page !== undefined ? (
+        <>
+          <Content html={page.html} />
+          <Actions
+            actions={page.actions}
+            setPage={setPage}
+            random={page.random}
+          />
+        </>
+      ) : (
+        <>
+          <div>
+            <h1>Error al seleccionar la página</h1>
+            <button
+              onClick={() => setPage(pages.intro)}
+              className="text-left w-[80%] border-l rounded-lg my-2 p-2 btn"
+            >
+              Ir al principio
+            </button>
+          </div>
+        </>
+      )}
     </main>
   );
 }
